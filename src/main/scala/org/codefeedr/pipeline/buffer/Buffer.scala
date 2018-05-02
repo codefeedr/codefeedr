082be3faee2a49244619e8323ed8d1a4a3c1a82e
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-02 10:35:25 +0200

    Fix build issues

--- src/main/scala/org/codefeedr/pipeline/buffer/Buffer.scala
+++ src/main/scala/org/codefeedr/pipeline/buffer/Buffer.scala
@@ -1,10 +1,10 @@
 package org.codefeedr.pipeline.buffer
 
-import org.apache.flink.api.java.operators.DataSink
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.DataStream
 import org.codefeedr.pipeline.Pipeline
 
 abstract class Buffer[T](pipeline: Pipeline) {
   def getSource: DataStream[T]
-  def getSink: DataSink[T]
+  def getSink: SinkFunction[T]
 }
--- src/main/scala/org/codefeedr/pipeline/buffer/FakeBuffer.scala
+++ src/main/scala/org/codefeedr/pipeline/buffer/FakeBuffer.scala
@@ -1,5 +1,6 @@
 package org.codefeedr.pipeline.buffer
 import org.apache.flink.api.java.operators.DataSink
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.DataStream
 import org.codefeedr.pipeline.Pipeline
 
@@ -7,5 +8,5 @@ import org.codefeedr.pipeline.Pipeline
 class FakeBuffer[T](pipeline: Pipeline) extends Buffer[T](pipeline) {
   override def getSource: DataStream[T] = ???
 
-  override def getSink: DataSink[T] = ???
+  override def getSink: SinkFunction[T] = ???
 }
