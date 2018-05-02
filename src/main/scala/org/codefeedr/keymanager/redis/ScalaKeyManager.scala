b609e26a71adfa7fa0f35cd6fa10f4889752b074
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-02 16:01:37 +0200

    Remove github empty plugin. Add redis keymanager class

---
+++ src/main/scala/org/codefeedr/keymanager/redis/ScalaKeyManager.scala
@@ -0,0 +1,7 @@
+package org.codefeedr.keymanager.redis
+
+import org.codefeedr.keymanager.KeyManager
+
+class ScalaKeyManager extends KeyManager {
+  override def request(target: String, numberOfCalls: Int): String = ???
+}
