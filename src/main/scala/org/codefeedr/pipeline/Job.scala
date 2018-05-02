47590760ac6e93699724e7b8567342b97deebeb3
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-02 14:58:03 +0200

    Attempt to check for NoType

--- src/main/scala/org/codefeedr/pipeline/Job.scala
+++ src/main/scala/org/codefeedr/pipeline/Job.scala
@@ -2,7 +2,9 @@ package org.codefeedr.pipeline
 
 import org.apache.flink.streaming.api.scala.DataStream
 
-abstract class Job[T <: PipelinedItem] extends PipelineObject[T, NoType] {
+import scala.reflect.ClassTag
+
+abstract class Job[T <: PipelinedItem : ClassTag] extends PipelineObject[T, NoType] {
 
   override def transform(source: DataStream[T]): DataStream[NoType] = {
     main(source)
--- src/main/scala/org/codefeedr/pipeline/PipelineObject.scala
+++ src/main/scala/org/codefeedr/pipeline/PipelineObject.scala
@@ -1,11 +1,14 @@
 package org.codefeedr.pipeline
 
+import com.sun.tools.javac.code.TypeTag
 import org.apache.flink.api.java.operators.DataSink
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.DataStream
 import org.codefeedr.pipeline.buffer.BufferFactory
 
-abstract class PipelineObject[In <: PipelinedItem, Out <: PipelinedItem] {
+import scala.reflect.{ClassTag, classTag}
+
+abstract class PipelineObject[In <: PipelinedItem : ClassTag, Out <: PipelinedItem : ClassTag] {
   var pipeline: Pipeline = null
 
   def setUp(pipeline: Pipeline): Unit = {
@@ -18,9 +21,20 @@ abstract class PipelineObject[In <: PipelinedItem, Out <: PipelinedItem] {
     this.pipeline = null
   }
 
+
+//  implicit object NoTypeSource extends PipelineObject[NoType, Out] {
+//    def smaller = (a:Int, b:Int) => (a < b)
+//  }
+
   // TODO: disallow In = NoType    (implict ev: In =:= NoType = null)
+//  def getSource[In: TypeTag]: DataStream[In] = {
   def getSource: DataStream[In] = {
     assert(pipeline != null)
+
+
+  if (classTag[In] == classTag[NoType]) {
+      print("NO TYPE SOURCE!")
+    }
 
     // Look up what source there is
     // if In == NoType then THROW
