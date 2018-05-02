335921dc8ac15604a74cd362019588dd41c068e4
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-02 10:27:09 +0200

    Fix new pipeline design in RSSSource

--- src/main/scala/org/codefeedr/plugins/rss/RSSSource.scala
+++ src/main/scala/org/codefeedr/plugins/rss/RSSSource.scala
@@ -1,9 +1,10 @@
 package org.codefeedr.plugins.rss
 
+import org.apache.flink.streaming.api.scala.DataStream
 import org.codefeedr.pipeline.{NoType, Pipeline, PipelineObject}
 
 class RSSSource(url: String) extends PipelineObject[NoType, RSSItem] {
-  override def main(): Unit = {
-
+  override def main(source: DataStream[NoType]): DataStream[RSSItem] = {
+    null
   }
 }
