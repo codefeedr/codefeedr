2d003ac3f58c33631cea5337806d57590647af13
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-04 16:56:50 +0200

    Start testing the pipeline builder

---
+++ src/test/scala/org/codefeedr/pipeline/PipelineBuilderTest.scala
@@ -0,0 +1,71 @@
+package org.codefeedr.pipeline
+
+import org.apache.flink.streaming.api.scala.DataStream
+import org.codefeedr.pipeline.buffer.BufferType
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
+
+class PipelineBuilderTest extends FunSuite with BeforeAndAfter with Matchers {
+
+  case class StringType(value: String) extends PipelinedItem
+
+  class EmptySourcePipelineObject extends PipelineObject[NoType, StringType] {
+    override def transform(source: DataStream[NoType]): DataStream[StringType] = ???
+  }
+
+  class EmptyTransformPipelineObject extends PipelineObject[StringType, StringType] {
+    override def transform(source: DataStream[StringType]): DataStream[StringType] = ???
+  }
+
+  class EmptySinkPipelineObject extends PipelineObject[StringType, NoType] {
+    override def transform(source: DataStream[StringType]): DataStream[NoType] = ???
+  }
+
+  var builder: PipelineBuilder = _
+
+  before {
+    builder = new PipelineBuilder()
+  }
+
+  test("An empty pipeline configuration throws") {
+    assertThrows[EmptyPipelineException] {
+      val pipeline = builder.build()
+    }
+  }
+
+  test("Set buffertype should also be retrievable") {
+    builder.setBufferType(BufferType.Kafka)
+
+    assert(builder.getBufferType == BufferType.Kafka)
+  }
+
+  test("Every pipeline object should appear in the pipeline (1)") {
+    val pipeline = builder
+      .add(new EmptySourcePipelineObject())
+      .build()
+
+    assert(pipeline.objects.length == 1)
+
+    pipeline.objects.head shouldBe an[EmptySourcePipelineObject]
+  }
+
+  test("Every pipeline object should appear in the pipeline (2)") {
+    val pipeline = builder
+      .add(new EmptySourcePipelineObject())
+      .add(new EmptyTransformPipelineObject())
+      .build()
+
+    assert(pipeline.objects.length == 2)
+
+    pipeline.objects.head shouldBe an[EmptySourcePipelineObject]
+    pipeline.objects.last shouldBe an[EmptyTransformPipelineObject]
+  }
+
+  test("Set properties should be available in pipeline properties") {
+    val pipeline = builder
+      .add(new EmptySourcePipelineObject())
+      .setProperty("key", "value")
+      .build()
+
+    assert(pipeline.properties.get("key") == "value")
+  }
+}
