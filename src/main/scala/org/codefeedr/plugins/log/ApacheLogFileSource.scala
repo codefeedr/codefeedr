b56e3dbca11973e48ed7b7d104afb84149f40a67
Joris Quist <Jorisquist@gmail.com>
2018-05-14 14:07:01 +0200

    Improvements suggested by Wouter

--- src/main/scala/org/codefeedr/plugins/log/ApacheLogFileSource.scala
+++ src/main/scala/org/codefeedr/plugins/log/ApacheLogFileSource.scala
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.codefeedr.plugins.log
 
 import java.time.LocalDateTime
@@ -6,6 +24,10 @@ import java.time.format.DateTimeFormatter
 import org.apache.flink.streaming.api.scala._
 import org.codefeedr.pipeline.Source
 
+/**
+  * Source that streams Apache log files
+  * @param absolutePath Absolute file path to the log
+  */
 class ApacheLogFileSource(absolutePath: String) extends Source[ApacheAccessLogItem] with Serializable {
 
   override def main(): DataStream[ApacheAccessLogItem] = {
@@ -37,6 +59,7 @@ class ApacheLogFileSource(absolutePath: String) extends Source[ApacheAccessLogIt
         toReturn = ApacheAccessLogItem(ipAddress, date, request, status, amountOfBytes, referer, userAgent)
 
       } catch {
+        // This means that a line couldn't be parsed with the regex, this means that an empty item will be returned
         case  _: Throwable =>
       }
 
