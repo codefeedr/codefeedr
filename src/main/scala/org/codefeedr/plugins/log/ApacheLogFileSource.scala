fc23e6df5071d6e8575c2c5112d1b5c28d51e161
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-15 13:09:24 +0200

    Rewrite logsource

--- src/main/scala/org/codefeedr/plugins/log/ApacheLogFileSource.scala
+++ src/main/scala/org/codefeedr/plugins/log/ApacheLogFileSource.scala
@@ -21,50 +21,40 @@ package org.codefeedr.plugins.log
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
 import org.codefeedr.pipeline.Source
 
 /**
   * Source that streams Apache log files
+  *
   * @param absolutePath Absolute file path to the log
   */
 class ApacheLogFileSource(absolutePath: String) extends Source[ApacheAccessLogItem] with Serializable {
 
   override def main(): DataStream[ApacheAccessLogItem] = {
-    val lineToLogItem = lineToLogItemFunction()
-    pipeline.environment.readTextFile(absolutePath).flatMap(_.split('\n')).map(v => lineToLogItem(v)) // lineToLogItem(v))
+    pipeline.environment
+      .readTextFile(absolutePath)
+      .flatMap(_.split('\n'))
+      .flatMap(new LogMapper())
   }
 
-  def lineToLogItemFunction(): String => ApacheAccessLogItem = {
-    line: String => {
-      val pattern = "^(\\S+) \\S+ \\S+ \\[([\\w:\\/]+\\s[+\\-]\\d{4})\\] \"(\\S+\\s*\\S+\\s*\\S+)?\\s*\" (\\d{3}) (\\S+) (\"[^\"]*\") (\"[^\"]*\") (\"[^\"]*\")".r
+}
 
-      val groups =  pattern.findFirstMatchIn(line).orNull
+private class LogMapper extends FlatMapFunction[String, ApacheAccessLogItem] {
 
-      var toReturn: ApacheAccessLogItem = ApacheAccessLogItem("", null, "", 0, 0, "", "")
+  lazy val dateFormatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z")
+  lazy val pattern = """^(\S+) \S+ \S+ \[([\w:\/]+\s[+\-]\d{4})\] "(\S+\s*\S+\s*\S+)?\s*" (\d{3}) (\S+) ("[^"]*") ("[^"]*") ("[^"]*")""".r
 
-      try {
-        val ipAddress = groups.group(1)
-        val dateString = groups.group(2)
-        val request = groups.group(3)
-        val status = if (groups.group(4).matches("^[0-9]+$")) groups.group(4).toInt else -1
-        val amountOfBytes =  if (groups.group(5).matches("^[0-9]+$")) groups.group(5).toInt else -1
-        val referer = groups.group(6)
-        val userAgent = groups.group(7)
+  def flatMap(line: String, out: Collector[ApacheAccessLogItem]): Unit = line match {
+    case pattern(ipAddress, dateString, request, status, amountOfBytes, referer, userAgent, _*) => {
+      val date = LocalDateTime.parse(dateString, dateFormatter)
 
-        // 06/Jan/2017:04:20:17 +0100
-        val formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z")
-        val date = LocalDateTime.parse(dateString, formatter)
-
-        toReturn = ApacheAccessLogItem(ipAddress, date, request, status, amountOfBytes, referer, userAgent)
-
-      } catch {
-        // This means that a line couldn't be parsed with the regex, this means that an empty item will be returned
-        case  _: Throwable =>
-      }
-
-      toReturn
+      val item = ApacheAccessLogItem(ipAddress, date, request, status.toInt, amountOfBytes.toInt, referer, userAgent)
+      out.collect(item)
     }
+    case _ => println("mismatch")
   }
 
 }
