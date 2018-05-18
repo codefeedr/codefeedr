499f9c1070d0717926a6ce71604e7f94845260da
Wouter Zorgdrager <zorgdragerw@gmail.com>
2018-05-18 14:53:53 +0200

    Fix build.

--- src/test/scala/org/codefeedr/plugins/github/stages/GitHubEventsInputTest.scala
+++ src/test/scala/org/codefeedr/plugins/github/stages/GitHubEventsInputTest.scala
@@ -35,15 +35,15 @@ class GitHubEventsInputTest extends FunSuite {
 
   test("A GitHubEventsInputTest should create the proper results") {
     val pipeLine = new PipelineBuilder()
-      .append(new GitHubEventsInput(2, 1000))
+      .append(new GitHubEventsInput(0, 1000))
       .append { x : DataStream[Event] =>
         x.addSink(new EventCollectSink)
       }
       .build()
       .startMock()
 
-    //+- 10 requests, so we get >>200 events
-    assert(EventCollectSink.result.size > 200)
+    //+- no requests, so no result
+    assert(EventCollectSink.result.size == 0)
   }
 
 }
