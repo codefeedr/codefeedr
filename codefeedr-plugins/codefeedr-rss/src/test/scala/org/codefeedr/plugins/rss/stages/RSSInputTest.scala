e8b5157b8de23ffd61cd8b0281cec1eb231f202a
Jos Kuijpers <jkuijpers@jarvix.nl>
2018-05-28 09:50:28 +0200

    [rss] Update naming to adhere to conventions

--- codefeedr-plugins/codefeedr-rss/src/test/scala/org/codefeedr/plugins/rss/stages/RSSInputTest.scala
+++ codefeedr-plugins/codefeedr-rss/src/test/scala/org/codefeedr/plugins/rss/stages/RSSInputTest.scala
@@ -1,20 +1,39 @@
-package org.codefeedr.plugins.rss
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codefeedr.plugins.rss.stages
 
 import org.apache.flink.api.common.JobID
 import org.apache.flink.runtime.client.JobExecutionException
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala._
 import org.codefeedr.pipeline.PipelineBuilder
+import org.codefeedr.plugins.rss.RSSItem
 import org.codefeedr.stages.OutputStage
 import org.scalatest.FunSuite
 
 //This will be thrown after the print sink received x elements.
 final case class JobFinishedException() extends JobExecutionException(new JobID(), "Job is finished.")
-class RSSInputStageTest extends FunSuite {
+class RSSInputTest extends FunSuite {
 
   test("RSS source end-to-end test") {
     val rssURL = "http://lorem-rss.herokuapp.com/feed?unit=second"
-    val source = new RSSInputStage(rssURL, "EEE, dd MMMM yyyy HH:mm:ss z", 1000)
+    val source = new RSSInput(rssURL, "EEE, dd MMMM yyyy HH:mm:ss z", 1000)
     val sink = new LimitingSinkPipelineObject(12)
 
     val pipeline = new PipelineBuilder()
