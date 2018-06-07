b3c3fb5a98838e2624bb94627f8bb0b41c1bc4b7
Wouter Zorgdrager <zorgdragerw@gmail.com>
2018-06-07 12:19:53 +0200

    [cargo] Repo name to filter on isn't hardcoded anymore, set `rust-lang/crates.io-index` as default.

--- codefeedr-plugins/codefeedr-cratesio/src/main/scala/org/codefeedr/plugins/cratesio/GitHubPushEventToCargoTransform.scala
+++ codefeedr-plugins/codefeedr-cratesio/src/main/scala/org/codefeedr/plugins/cratesio/GitHubPushEventToCargoTransform.scala
@@ -6,15 +6,21 @@ import org.codefeedr.plugins.github.GitHubProtocol.PushEvent
 import org.apache.flink.api.scala._
 import org.codefeedr.stages.TransformStage
 
-class GitHubPushEventToCargoTransform extends TransformStage[PushEvent, CrateInfo] {
+class GitHubPushEventToCargoTransform(repoName : String = "rust-lang/crates.io-index") extends TransformStage[PushEvent, CrateInfo] {
 
+  /**
+    * Transfrom PushEvents into CrateInfo.
+    *
+    * @param source a push_event stream.
+    * @return a stream of crateinfos.
+    */
   override def transform(source: DataStream[PushEvent]): DataStream[CrateInfo] = {
 
     val service = new CratesIOAPIService()
 
     source.
-      filter(e => e.repo.name == "rust-lang/crates.io-index").
-      flatMap(e => e.payload.commits.map(c => service.extactCrateFromCommitMsg(c.message))).
+      filter(e => e.repo.name == repoName).
+      flatMap(e => e.payload.commits.map(c => service.extractCrateFromCommitMsg(c.message))).
       filter (t => t.isSuccess).
       map(crate => service.crateInfo(crate.get))
   }
