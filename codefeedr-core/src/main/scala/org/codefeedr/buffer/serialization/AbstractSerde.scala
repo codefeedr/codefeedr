c3fa4863c212054be8dc209d863e5b092f641fc6
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-01-29 10:53:35 +0100

    Fixed typeinformation bug in AbstractSerde.

--- codefeedr-core/src/main/scala/org/codefeedr/buffer/serialization/AbstractSerde.scala
+++ codefeedr-core/src/main/scala/org/codefeedr/buffer/serialization/AbstractSerde.scala
@@ -24,7 +24,7 @@ import org.apache.flink.api.java.typeutils.TypeExtractor
 
 import scala.reflect.{ClassTag, classTag}
 
-abstract class AbstractSerde[T <: Serializable : ClassTag] extends AbstractDeserializationSchema[T] with SerializationSchema[T] {
+abstract class AbstractSerde[T <: Serializable : ClassTag]() extends AbstractDeserializationSchema[T](TypeExtractor.createTypeInfo(classTag[T].runtimeClass.asInstanceOf[Class[T]])) with SerializationSchema[T] {
 
   // Get type of class
   val inputClassType: Class[T] = classTag[T].runtimeClass.asInstanceOf[Class[T]]
