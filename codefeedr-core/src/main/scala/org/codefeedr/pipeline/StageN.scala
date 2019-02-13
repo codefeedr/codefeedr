a37b96fd6b3e9999b3991a1b891b5e8b3966708f
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-02-13 14:24:42 +0100

    Refactored StageN.

--- codefeedr-core/src/main/scala/org/codefeedr/pipeline/StageN.scala
+++ codefeedr-core/src/main/scala/org/codefeedr/pipeline/StageN.scala
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.codefeedr.pipeline
 
 import org.apache.flink.streaming.api.scala.DataStream
@@ -24,12 +23,11 @@ import org.codefeedr.stages.StageAttributes
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
 
-/**
-  * A pipeline object with 2 sources and 1 output
+/** A stage with 2 sources and 1 output.
   *
-  * @tparam In  input type for this pipeline object.
-  * @tparam In2 second input type for this pipeline object.
-  * @tparam Out output type for this pipeline object.
+  * @tparam In  Input type for this stage.
+  * @tparam In2 Second input type for this stage.
+  * @tparam Out Output type for this stage.
   */
 abstract class Stage2[In <: Serializable with AnyRef: ClassTag: TypeTag,
 In2 <: Serializable with AnyRef: ClassTag: TypeTag,
@@ -37,13 +35,23 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     attributes: StageAttributes = StageAttributes())
     extends Stage[In, Out](attributes) {
 
+  /** Transforms from type In to type Out.
+    *
+    * @param source The input source with type In.
+    * @return The transformed stream with type Out.
+    */
   override def transform(source: DataStream[In]): DataStream[Out] =
     transform(source, getSource[In2](getParents(1)))
 
+  /** Verifies this stage in the graph.
+    * Checks if the stage has enough parents and input types are no NoTypes.
+    *
+    * @param graph The graph the stage is in.
+    */
   override def verifyGraph(graph: DirectedAcyclicGraph): Unit = {
     if (typeOf[In] == typeOf[NoType] || typeOf[In2] == typeOf[NoType]) {
       throw new IllegalStateException(
-        "Cannot use NoType on pipeline objects with multiple input sources")
+        "Cannot use NoType on stages with multiple input sources")
     }
 
     if (graph.getParents(this).size < 2) {
@@ -52,25 +60,23 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     }
   }
 
-  /**
-    * Transform two input streams into a single output stream.
+  /** Transform two input streams into a single output stream.
     *
-    * @param source Source stream
-    * @param secondSource Second source stream
-    * @return An output stream
+    * @param source The (main) source.
+    * @param secondSource The second source.
+    * @return An output stream.
     */
   def transform(source: DataStream[In],
                 secondSource: DataStream[In2]): DataStream[Out]
 
 }
 
-/**
-  * A pipeline object with 3 sources and 1 output
+/** A stage with 3 sources and 1 output
   *
-  * @tparam In  input type for this pipeline object.
-  * @tparam In2 second input type for this pipeline object.
-  * @tparam In3 third input type of this pipeline object.
-  * @tparam Out output type for this pipeline object.
+  * @tparam In  Input type for this stage.
+  * @tparam In2 Second input type for this stage.
+  * @tparam In3 Third input type of this stage.
+  * @tparam Out Output type for this stage.
   */
 abstract class Stage3[In <: Serializable with AnyRef: ClassTag: TypeTag,
 In2 <: Serializable with AnyRef: ClassTag: TypeTag,
@@ -79,16 +85,27 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     attributes: StageAttributes = StageAttributes())
     extends Stage2[In, In2, Out](attributes) {
 
+  /** Transforms from type In and In2 to type Out.
+    *
+    * @param source The input source with type In.
+    * @param secondSource The input source with type In2.
+    * @return The transformed stream with type Out.
+    */
   override def transform(source: DataStream[In],
                          secondSource: DataStream[In2]): DataStream[Out] =
     transform(source, secondSource, getSource[In3](getParents(2)))
 
+  /** Verifies this stage in the graph.
+    * Checks if the stage has enough parents and input types are no NoTypes.
+    *
+    * @param graph The graph the stage is in.
+    */
   override def verifyGraph(graph: DirectedAcyclicGraph): Unit = {
     super.verifyGraph(graph)
 
     if (typeOf[In3] == typeOf[NoType]) {
       throw new IllegalStateException(
-        "Cannot use NoType on pipeline objects with multiple input sources")
+        "Cannot use NoType on stages with multiple input sources")
     }
 
     if (graph.getParents(this).size < 3) {
@@ -97,13 +114,12 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     }
   }
 
-  /**
-    * Transform three input streams into a single output stream.
+  /** Transform three input streams into a single output stream.
     *
-    * @param source Source stream
-    * @param secondSource Second source stream
-    * @param thirdSource Third source stream
-    * @return An output stream
+    * @param source The (main) source.
+    * @param secondSource The second source.
+    * @param thirdSource The third source.
+    * @return An output stream.
     */
   def transform(source: DataStream[In],
                 secondSource: DataStream[In2],
@@ -111,14 +127,13 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
 
 }
 
-/**
-  * A pipeline object with 4 sources and 1 output
+/** A stage with 4 sources and 1 output.
   *
-  * @tparam In  input type for this pipeline object.
-  * @tparam In2 second input type for this pipeline object.
-  * @tparam In3 third input type of this pipeline object.
-  * @tparam In4 fourth input type of this pipeline object.
-  * @tparam Out output type for this pipeline object.
+  * @tparam In  Input type for this stage.
+  * @tparam In2 Second input type for this stage.
+  * @tparam In3 Third input type of this stage.
+  * @tparam In4 Fourth input type of this stage.
+  * @tparam Out Output type for this stage.
   */
 abstract class Stage4[In <: Serializable with AnyRef: ClassTag: TypeTag,
 In2 <: Serializable with AnyRef: ClassTag: TypeTag,
@@ -128,17 +143,29 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     attributes: StageAttributes = StageAttributes())
     extends Stage3[In, In2, In3, Out](attributes) {
 
+  /** Transforms from type In, In2, In3 to type Out.
+    *
+    * @param source The input source with type In.
+    * @param secondSource The input source with type In2.
+    * @param thirdSource The input source with type In3.
+    * @return The transformed stream with type Out.
+    */
   override def transform(source: DataStream[In],
                          secondSource: DataStream[In2],
                          thirdSource: DataStream[In3]): DataStream[Out] =
     transform(source, secondSource, thirdSource, getSource[In4](getParents(3)))
 
+  /** Verifies this stage in the graph.
+    * Checks if the stage has enough parents and input types are no NoTypes.
+    *
+    * @param graph The graph the stage is in.
+    */
   override def verifyGraph(graph: DirectedAcyclicGraph): Unit = {
     super.verifyGraph(graph)
 
     if (typeOf[In4] == typeOf[NoType]) {
       throw new IllegalStateException(
-        "Cannot use NoType on pipeline objects with multiple input sources")
+        "Cannot use NoType on stages with multiple input sources")
     }
 
     if (graph.getParents(this).size < 4) {
@@ -147,14 +174,13 @@ Out <: Serializable with AnyRef: ClassTag: TypeTag](
     }
   }
 
-  /**
-    * Transform four input streams into a single output stream.
+  /** Transform four input streams into a single output stream.
     *
-    * @param source Source stream
-    * @param secondSource Second source stream
-    * @param thirdSource Third source stream
-    * @param fourthSource Fourth source stream
-    * @return An output stream
+    * @param source The (main) source.
+    * @param secondSource The second source.
+    * @param thirdSource The third source.
+    * @param fourthSource The fourth source.
+    * @return An output stream.
     */
   def transform(source: DataStream[In],
                 secondSource: DataStream[In2],
