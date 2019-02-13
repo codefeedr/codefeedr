f237a333d21c5e614c06bdb80f3fd951d1291f7e
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-02-13 10:54:53 +0100

    Finalize Pipeline refactoring.

--- codefeedr-core/src/main/scala/org/codefeedr/pipeline/Pipeline.scala
+++ codefeedr-core/src/main/scala/org/codefeedr/pipeline/Pipeline.scala
@@ -27,6 +27,13 @@ import org.codefeedr.buffer.BufferType.BufferType
 import org.codefeedr.keymanager.KeyManager
 import org.codefeedr.pipeline.RuntimeType.RuntimeType
 
+/** Properties of a pipeline are stored in this case class.
+  *
+  * @param bufferType The type of [[org.codefeedr.buffer.Buffer]] (e.g. Kafka).
+  * @param bufferProperties The properties of the Buffer.
+  * @param keyManager The key manager which provide API call management at stage-level.
+  * @param streamTimeCharacteristic The TimeCharacteristic of the whole pipeline. Event, Ingestion or Processing.
+  */
 case class PipelineProperties(bufferType: BufferType,
                               bufferProperties: Properties,
                               keyManager: KeyManager,
@@ -36,10 +43,7 @@ case class PipelineProperties(bufferType: BufferType,
   * It stores all stages (Flink jobs) and connects them by setting up buffers (like Kafka).
   *
   * @param name The name of the pipeline.
-  * @param bufferType The type of [[org.codefeedr.buffer.Buffer]] (e.g. Kafka).
-  * @param bufferProperties The properties of the Buffer.
   * @param graph The graph of stages (nodes) and edges (buffers).
-  * @param keyManager The key manager which provide API call management at stage-level.
   * @param objectProperties the properties of each stage.
   */
 case class Pipeline(var name: String,
@@ -73,38 +77,43 @@ case class Pipeline(var name: String,
   /** Auxiliary method to retrieve key manager. */
   def keyManager = pipelineProperties.keyManager
 
-  /**
-    * Get the properties of a stage
+  /** Get the properties of a stage.
     *
-    * @param obj Stage
-    * @return Properties
+    * @param stage Stage The stage to retrieve properties from.
+    * @return Properties The obtained properties.
     */
   def propertiesOf[U <: Serializable with AnyRef,
                    V <: Serializable with AnyRef](
-      obj: Stage[U, V]): Properties = {
-    if (obj == null) {
-      throw new IllegalArgumentException("Object can't be null")
+      stage: Stage[U, V]): Properties = {
+    if (stage == null) {
+      throw new IllegalArgumentException("Stage can't be null")
     }
 
-    objectProperties.getOrElse(obj.id, new Properties())
+    objectProperties.getOrElse(stage.id, new Properties())
   }
 
-  /**
-    * Start the pipeline with a list of command line arguments
+  /** Start the pipeline with a list of command line arguments.
     *
-    * @param args Command line arguments
+    * Three types of run-modes are available:
+    * - Mock: Flink Jobs are linked without buffer, this requires the pipeline to be sequential.
+    * - Local: All Flink jobs are run in one [[StreamExecutionEnvironment]] connected with buffers.
+    * - Cluster: Flink jobs are run separately and connected with buffers.
+    *
+    * @param args Array of command line arguments.
     */
   def start(args: Array[String]): Unit = {
     val params = ParameterTool.fromArgs(args)
+
+    // By default it is local.
     var runtime = RuntimeType.Local
 
-    // If a pipeline object is specified, set to cluster
+    // If a stage is specified, set to cluster-mode.
     val stage = params.get("stage")
     if (stage != null) {
       runtime = RuntimeType.Cluster
     }
 
-    // Override runtime with runtime parameter
+    // Override runtime with runtime parameter.
     runtime = params.get("runtime") match {
       case "mock"    => RuntimeType.Mock
       case "local"   => RuntimeType.Local
@@ -112,11 +121,11 @@ case class Pipeline(var name: String,
       case _         => runtime
     }
 
-    // Set name if specified
+    // Set name if specified.
     name = params.get("name", name)
 
     if (params.has("list") || runtime == RuntimeType.Cluster) {
-      validateUniqueness()
+      validateUniqueness() // make sure stage id's are unique.
     }
 
     if (params.has("list")) {
@@ -126,27 +135,27 @@ case class Pipeline(var name: String,
     }
   }
 
+  /** Get all the nodes/stages in the pipeline. */
   private def getNodes
     : Vector[Stage[Serializable with AnyRef, Serializable with AnyRef]] =
     graph.nodes.asInstanceOf[Vector[
       Stage[Serializable with AnyRef, Serializable with AnyRef]]]
 
-  /**
-    * Validates the uniqueness of the stage IDs, needed for clustered running
-    */
+  /** Validates the uniqueness of the stage IDs, needed for clustered running. */
   def validateUniqueness(): Unit = {
     val list = getNodes.map(_.id)
     val uniqList = list.distinct
     val overlap = list.diff(uniqList)
 
+    // If overlap, throw exception.
     if (overlap.nonEmpty) {
       throw StageIdsNotUniqueException(overlap.head)
     }
   }
 
-  /**
-    * Shows a list of stages inside the pipeline. Option to throw an exception to get the data through Flink.
-    * @param asException
+  /** Shows a list of stages inside the pipeline. Option to throw an exception to get the data through Flink.
+    *
+    * @param asException Throws the list as an exception.
     */
   def showList(asException: Boolean): Unit = {
     if (asException) {
@@ -163,11 +172,11 @@ case class Pipeline(var name: String,
     }
   }
 
-  /**
-    * Start the pipeline with a run configuration
+  /** Start the pipeline with a run configuration.
     *
-    * @param runtime Runtime type
-    * @param stage   Stage of a cluster run
+    * @param runtime Runtime mode (mock, local, clustered).
+    * @param stage   Stage to start in case of a clustered run.
+    * @param groupId Group id of the stage (by default set to stage id).
     */
   def start(runtime: RuntimeType,
             stage: String = null,
@@ -179,95 +188,104 @@ case class Pipeline(var name: String,
     }
   }
 
-  /**
-    * Run the pipeline as mock. Only works for sequential pipelines.
+  /** Run the pipeline as mock. Only works for sequential pipelines.
     *
     * In a mock run, all stages are put together without buffers and run as a single Flink job.
     */
   def startMock(): Unit = {
     if (!graph.isSequential) {
       throw new IllegalStateException(
-        "Mock runtime can't run non-sequential pipelines")
+        "Mock mode can't run non-sequential pipelines.")
     }
 
-    val objects = getNodes
+    val nodes = getNodes
 
-    // Run all setups
-    for (obj <- objects) {
-      obj.setUp(this)
+    // Run all setups.
+    for (nodes <- nodes) {
+      nodes.setUp(this)
     }
 
     // Connect each object by getting a starting buffer, if any, and sending it to the next.
     var buffer: DataStream[Serializable with AnyRef] = null
-    for (obj <- objects) {
+    for (obj <- nodes) {
       buffer = obj.transform(buffer)
     }
 
+    // Execute in one environment.
     environment.execute(s"$name: mock")
   }
 
-  /**
-    * Start a locally run pipeline.
-    *
-    * Starts every stage in the same Flink environment but with buffers.
+  /** Start a locally run pipeline.
+    * This mode starts every stage in the same Flink environment but with buffers.
     */
   def startLocal(): Unit = {
-    val objects = getNodes
+    val nodes = getNodes
 
-    // Run all setups
-    for (obj <- objects) {
+    // Run all setups.
+    for (obj <- nodes) {
       obj.setUp(this)
     }
 
-    // For each PO, make buffers and run
-    for (obj <- objects) {
-      runObject(obj)
+    // For each PO, make buffers and run.
+    for (obj <- nodes) {
+      runStage(obj)
     }
 
+    // Execute in one environment.
     environment.execute(s"$name: local")
   }
 
-  /**
-    * Run the pipeline in a clustered manner: run a single stage only.
+  /** Run the pipeline in a clustered manner: run a single stage only.
     *
-    * @param stage Stage to run
+    * @param stage Stage to run.
+    * @param groupId GroupId to set.
     */
   def startClustered(stage: String, groupId: String = null): Unit = {
+    // Find correct stage.
     val optObj = graph.nodes.find { node =>
       node
         .asInstanceOf[Stage[Serializable with AnyRef, Serializable with AnyRef]]
         .id == stage
     }
 
+    // If cannot be find, throw an exception.
     if (optObj.isEmpty) {
-      throw StageNotFoundException()
+      throw StageNotFoundException(s"Stage with name: $stage not found.")
     }
 
+    // Cast to a stage.
     val obj = optObj.get
       .asInstanceOf[Stage[Serializable with AnyRef, Serializable with AnyRef]]
 
+    // Setup and run object.
     obj.setUp(this)
-    runObject(obj, groupId)
+    runStage(obj, groupId)
 
+    // Run stage in one environment.
     environment.execute(s"$name: ${stage}")
   }
 
-  /**
-    * Run a pipeline object.
-    *
+  /** Runs a stage.
     * Creates a source and sink for the object and then runs the transform function.
     *
-    * @param obj
+    * @param stage The stage to run.
+    * @param groupId The group id to set for the stage.
     */
-  private def runObject(
-      obj: Stage[Serializable with AnyRef, Serializable with AnyRef],
+  private def runStage(
+      stage: Stage[Serializable with AnyRef, Serializable with AnyRef],
       groupId: String = null): Unit = {
+
+    // Find the source lazily.
     lazy val source =
-      if (obj.hasMainSource) obj.getMainSource(groupId) else null
-    lazy val sink = if (obj.hasSink) obj.getSink(groupId) else null
+      if (stage.hasMainSource) stage.getMainSource(groupId) else null
 
-    val transformed = obj.transform(source)
+    // Find the sink lazily.
+    lazy val sink = if (stage.hasSink) stage.getSink(groupId) else null
 
+    // Add source.
+    val transformed = stage.transform(source)
+
+    // Add sink.
     if (sink != null) {
       transformed.addSink(sink)
     }
