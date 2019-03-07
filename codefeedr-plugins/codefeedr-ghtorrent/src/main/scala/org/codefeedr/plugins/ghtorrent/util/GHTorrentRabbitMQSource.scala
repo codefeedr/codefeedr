db39b63f12236cd2de099e90181fbe948457f51e
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-03-07 14:20:04 +0100

    Setup custom source.

--- codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/util/GHTorrentRabbitMQSource.scala
+++ codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/util/GHTorrentRabbitMQSource.scala
@@ -1,3 +1,15 @@
 package org.codefeedr.plugins.ghtorrent.util
 
-class GHTorrentRabbitMQSource {}
+import org.apache.flink.api.common.serialization.DeserializationSchema
+import org.apache.flink.streaming.connectors.rabbitmq.RMQSource
+import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig
+
+class GHTorrentRMQSource[T](rmqConnectionConfig: RMQConnectionConfig,
+                            queueName: String,
+                            deserSchema: DeserializationSchema[T])
+    extends RMQSource[T](rmqConnectionConfig, queueName, deserSchema) {
+
+  override def setupQueue(): Unit = {
+    //setup queue according to http://ghtorrent.org/streaming.html
+  }
+}
