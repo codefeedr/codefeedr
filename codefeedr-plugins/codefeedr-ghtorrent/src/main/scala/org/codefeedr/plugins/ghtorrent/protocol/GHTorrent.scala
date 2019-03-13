99719f881009388bb7561b445d022648a95d329b
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-03-13 16:39:43 +0100

    Added more Scaladoc.

--- codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/protocol/GHTorrent.scala
+++ codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/protocol/GHTorrent.scala
@@ -2,5 +2,10 @@ package org.codefeedr.plugins.ghtorrent.protocol
 
 object GHTorrent {
 
+  /** Represents a record as retrieved from GHTorrent.
+    *
+    * @param routingKey the routing key (e.g. evt.push.insert)
+    * @param contents the content of the queue record.
+    */
   case class Record(routingKey: String, contents: String)
 }
