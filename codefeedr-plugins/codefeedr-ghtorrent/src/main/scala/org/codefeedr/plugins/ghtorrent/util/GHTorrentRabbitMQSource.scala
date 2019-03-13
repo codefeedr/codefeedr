a2d8242899ae05e83674d2f6f4006d63f6e883aa
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-03-13 10:39:24 +0100

    Started implementing custom RabbitMQ source.

--- codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/util/GHTorrentRabbitMQSource.scala
+++ codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/util/GHTorrentRabbitMQSource.scala
@@ -1,13 +1,24 @@
 package org.codefeedr.plugins.ghtorrent.util
 
+import com.rabbitmq.client.ConnectionFactory
 import org.apache.flink.api.common.serialization.DeserializationSchema
 import org.apache.flink.streaming.connectors.rabbitmq.RMQSource
 import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig
+import org.codefeedr.plugins.ghtorrent.protocol.GHTorrent.Record
 
-class GHTorrentRMQSource[T](rmqConnectionConfig: RMQConnectionConfig,
-                            queueName: String,
-                            deserSchema: DeserializationSchema[T])
-    extends RMQSource[T](rmqConnectionConfig, queueName, deserSchema) {
+class GHTorrentRMQSource(deserSchema: DeserializationSchema[Record])
+    extends RMQSource[Record](
+      new RMQConnectionConfig.Builder()
+        .setHost("localhost")
+        .setUserName("streamer")
+        .setPassword("streamer")
+        .build(),
+      "", //Set the queue_name to "" since we are going to override it anyways.
+      deserSchema
+    ) {
+
+  /** Name of the exchange, this is a requirement by the GHTorrent streaming service. **/
+  val exchangeName = "ght-streams"
 
   override def setupQueue(): Unit = {
     //setup queue according to http://ghtorrent.org/streaming.html
