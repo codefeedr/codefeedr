87559da2afe3d2bab3933c42cd431cf4c8c59c1f
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-03-14 10:32:42 +0100

    Added Apache license.

---
+++ codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/protocol/GitHub.scala
@@ -0,0 +1,57 @@
+package org.codefeedr.plugins.ghtorrent.protocol
+
+object GitHub {
+
+  /**
+    * START Commit
+    */
+  case class Commit(sha: String,
+                    url: String,
+                    commit: CommitData,
+                    author: Option[User],
+                    committer: Option[User],
+                    parents: List[Parent],
+                    stats: Stats,
+                    files: List[File])
+
+  case class CommitData(author: CommitUser,
+                        committer: CommitUser,
+                        message: String,
+                        tree: Tree,
+                        comment_count: Int,
+                        verification: Verification)
+
+  case class CommitUser(name: String, email: String, date: String)
+
+  case class User(id: Long,
+                  login: String,
+                  avatar_url: String,
+                  `type`: String,
+                  site_admin: Boolean)
+
+  case class Verification(verified: Boolean,
+                          reason: String,
+                          signature: Option[String],
+                          payload: Option[String])
+
+  case class Stats(total: Int, additions: Int, deletions: Int)
+
+  case class File(sha: Option[String],
+                  filename: Option[String],
+                  status: Option[String],
+                  additions: Int,
+                  deletions: Int,
+                  changes: Int,
+                  blob_url: Option[String],
+                  raw_url: Option[String],
+                  contents_url: Option[String],
+                  patch: Option[String])
+
+  case class Parent(sha: String)
+
+  case class Tree(sha: String)
+
+  /**
+  * END Commit
+  */
+}
