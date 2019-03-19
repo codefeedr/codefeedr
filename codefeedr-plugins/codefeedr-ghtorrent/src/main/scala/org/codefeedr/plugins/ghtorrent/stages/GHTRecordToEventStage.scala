0eb9fd57a3a96ec1017f092f94d9f38f2645d1f1
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-03-19 15:58:12 +0100

    Added side output if parsing fails.

--- codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/stages/GHTRecordToEventStage.scala
+++ codefeedr-plugins/codefeedr-ghtorrent/src/main/scala/org/codefeedr/plugins/ghtorrent/stages/GHTRecordToEventStage.scala
@@ -1,14 +1,29 @@
 package org.codefeedr.plugins.ghtorrent.stages
 
-import org.apache.flink.api.common.functions.{FilterFunction, MapFunction}
+import java.util.Properties
+
+import org.apache.flink.api.common.functions.{
+  FilterFunction,
+  MapFunction,
+  RichMapFunction
+}
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag}
 import org.codefeedr.plugins.ghtorrent.protocol.GHTorrent.{Event, Record}
 import org.codefeedr.stages.TransformStage
 import org.json4s.DefaultFormats
 import org.json4s.ext.JavaTimeSerializers
 import org.json4s.jackson.JsonMethods.parse
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011
+import org.apache.flink.streaming.connectors.kafka.internal.FlinkKafkaProducer
+import org.apache.flink.types.Nothing
+import org.apache.flink.util.Collector
+import org.codefeedr.buffer.serialization.Serializer
+import org.codefeedr.stages.kafka.KafkaOutput
+import org.slf4j.Logger
+
 import scala.reflect.{ClassTag, classTag}
 import scala.reflect.runtime.universe._
 
@@ -21,8 +36,13 @@ import scala.reflect.runtime.universe._
 class GHTRecordToEventStage[
     T <: Serializable with AnyRef with Event: TypeTag: ClassTag: TypeInformation](
     stageName: String,
-    var routingKey: String)
+    routingKey: String,
+    sideOutput: Boolean = true,
+    sideOutputTopic: String = "parse_exception",
+    sideOutputKafkaServer: String = "localhost:9092")
     extends TransformStage[Record, T] {
+
+  val outputTag = OutputTag[Record]("parse_exception")
 
   /** Transforms and parses [[Event]] from [[Record]].
     *
@@ -30,22 +50,41 @@ class GHTRecordToEventStage[
     * @return The transformed stream with type T.
     */
   override def transform(source: DataStream[Record]): DataStream[T] = {
-    source
-      .filter(new RoutingFilter(routingKey))
-      .map(new EventExtract[T])
+    val trans = source
+      .process(new EventExtract[T](routingKey, outputTag))
+
+    if (sideOutput) {
+      trans
+        .getSideOutput(outputTag)
+        .addSink(
+          new FlinkKafkaProducer011[Record](
+            sideOutputKafkaServer,
+            sideOutputTopic,
+            Serializer.getSerde[Record](Serializer.JSON)))
+    }
+
+    trans
   }
 
 }
 
-/** Filters based on a routing key. */
-class RoutingFilter(routingKey: String) extends FilterFunction[Record] {
-  override def filter(value: Record): Boolean = value.routingKey == routingKey
-}
+/** Filters on routing key and extracts */
+class EventExtract[T: Manifest](routingKey: String,
+                                outputTag: OutputTag[Record])
+    extends ProcessFunction[Record, T] {
 
-/** Extracts an event based on the type */
-class EventExtract[T: Manifest] extends MapFunction[Record, T] {
-  override def map(value: Record): T = {
-    implicit val defaultFormats = DefaultFormats ++ JavaTimeSerializers.all
-    parse(value.contents).extract[T]
+  implicit lazy val defaultFormats = DefaultFormats ++ JavaTimeSerializers.all
+
+  override def processElement(value: Record,
+                              ctx: ProcessFunction[Record, T]#Context,
+                              out: Collector[T]): Unit = {
+    if (value.routingKey != routingKey) return //filter on routing keys
+    val parsedEvent = parse(value.contents).extract[T]
+
+    try {
+      out.collect(parsedEvent)
+    } catch {
+      case e: Exception => ctx.output(outputTag, value)
+    }
   }
 }
