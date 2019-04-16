293bc1721d59ef34f8faf602be6f97b7600e01f1
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-16 12:36:54 +0200

    Started on release source.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/util/PyPiReleasesSource.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/util/PyPiReleasesSource.scala
@@ -1,11 +1,48 @@
 package org.codefeedr.plugins.pypi.util
 
-import org.apache.flink.streaming.api.functions.source.SourceFunction
+import java.net.URL
+
+import com.rometools.rome.feed.synd.SyndFeed
+import com.rometools.rome.io.{SyndFeedInput, XmlReader}
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.source.{
+  RichSourceFunction,
+  SourceFunction
+}
 import org.codefeedr.plugins.pypi.protocol.Protocol.PyPiRelease
 
-class PyPiReleasesSource extends SourceFunction[PyPiRelease] {
+class PyPiReleasesSource(waitTime: Long = 1000)
+    extends RichSourceFunction[PyPiRelease] {
 
-  override def run(ctx: SourceFunction.SourceContext[PyPiRelease]): Unit = ???
+  val feedUrl = new URL("https://pypi.org/rss/updates.xml")
 
-  override def cancel(): Unit = ???
+  var input: SyndFeedInput = _
+  var feed: SyndFeed = _
+  var titleState: ListState[String] = _
+  var isRunning = false
+
+  override def open(parameters: Configuration): Unit = {
+    input = new SyndFeedInput()
+    feed = input.build(new XmlReader(feedUrl))
+
+    val titleStateDescriptor =
+      new ListStateDescriptor[String]("titles_already_send", classOf[String])
+
+    titleState = getRuntimeContext.getListState(titleStateDescriptor)
+
+    isRunning = true
+
+  }
+
+  override def run(ctx: SourceFunction.SourceContext[PyPiRelease]): Unit = {
+    while (isRunning) {
+
+      Thread.sleep(waitTime)
+    }
+
+    cancel()
+  }
+
+  override def cancel(): Unit = isRunning = false
 }
