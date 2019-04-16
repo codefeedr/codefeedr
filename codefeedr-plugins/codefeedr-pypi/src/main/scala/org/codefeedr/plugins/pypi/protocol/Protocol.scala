036d08ddfa67a25d92d2e01245ce9cbe067f4ba4
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-16 16:20:03 +0200

    Added project protocol.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/protocol/Protocol.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/protocol/Protocol.scala
@@ -9,4 +9,59 @@ object Protocol {
                          description: String,
                          pubDate: Date)
 
+  case class PyPiReleaseExt(title: String,
+                            link: String,
+                            description: String,
+                            pubDate: Date,
+                            project: PyPiProject)
+
+  case class PyPiProject(info: Info,
+                         last_serial: Long,
+                         releases: List[ReleaseVersion],
+                         urls: List[Release])
+
+  case class Info(author: String,
+                  author_email: String,
+                  bugtrack_url: Option[String],
+                  classifiers: List[String],
+                  description: String,
+                  description_content_type: String,
+                  docs_url: String,
+                  download_url: String,
+                  downloads: Downloads,
+                  home_page: String,
+                  keywords: String,
+                  license: String,
+                  maintainer: String,
+                  maintainer_email: String,
+                  name: String,
+                  package_url: String,
+                  platform: String,
+                  project_url: String,
+                  project_urls: ProjectUrl,
+                  release_url: String,
+                  requires_dist: String,
+                  requires_python: String,
+                  summary: String,
+                  version: String)
+
+  case class Downloads(last_day: Int, last_month: Int, last_week: Int)
+  case class ProjectUrl(Homepage: String)
+
+  case class ReleaseVersion(version: String, releases: List[Release])
+
+  case class Release(comment_text: String,
+                     digests: Digest,
+                     downloads: Double,
+                     filename: String,
+                     has_sig: Boolean,
+                     md5_digest: String,
+                     packagetype: String,
+                     python_version: String,
+                     requires_python: String,
+                     size: Double,
+                     upload_time: Date,
+                     url: String)
+
+  case class Digest(md5: String, sha256: String)
 }
