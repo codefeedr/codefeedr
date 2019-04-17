c802f7bd2ff6be63bc984f34a4414e59c445bedf
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 11:45:37 +0200

    Finished core-code.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
@@ -8,8 +8,10 @@ import org.codefeedr.plugins.pypi.protocol.Protocol.{PyPiProject, PyPiRelease}
 import org.codefeedr.stages.TransformStage
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.async.{AsyncFunction, ResultFuture}
+import org.codefeedr.plugins.pypi.util.PyPiService
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 class PyPiReleaseExtStage(stageId: String = "pypi_releases")
     extends TransformStage[PyPiRelease, PyPiProject](Some(stageId)) {
   override def transform(
@@ -29,7 +31,22 @@ class MapReleaseToProject extends AsyncFunction[PyPiRelease, PyPiProject] {
   implicit val executor: ExecutionContext = ExecutionContext.global
 
   override def asyncInvoke(input: PyPiRelease,
-                           resultFuture: ResultFuture[PyPiProject]): Unit = {}
+                           resultFuture: ResultFuture[PyPiProject]): Unit = {
+    val projectName = input.title.split(" ")(0)
+
+    val requestProject: Future[Option[PyPiProject]] = Future(
+      PyPiService.getProject(projectName))
+
+    requestProject.onComplete {
+      case Success(result: Option[PyPiRelease]) => {
+        if (result.isDefined) resultFuture.complete(Iterable(result.get))
+      }
+      case Failure(e) =>
+        resultFuture.complete(Iterable())
+        e.printStackTrace()
+    }
+
+  }
 
   override def timeout(input: PyPiRelease,
                        resultFuture: ResultFuture[PyPiProject]): Unit = {}
--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/util/PyPiService.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/util/PyPiService.scala
@@ -8,11 +8,16 @@ import scalaj.http.{Http, HttpRequest}
 import org.json4s.jackson.JsonMethods.parse
 import org.json4s.Extraction._
 
-class PyPiService extends Serializable {
+object PyPiService extends Serializable {
 
   lazy implicit val formats: Formats = DefaultFormats ++ JavaTimeSerializers.all
   private val url = "https://pypi.org/pypi/"
 
+  /** Retrieves a PyPi project.
+    *
+    * @param projectName the name of the project.
+    * @return an optional PyPiProject.
+    */
   def getProject(projectName: String): Option[PyPiProject] = {
     val projectEndPoint = projectName + "/json"
 
@@ -24,6 +29,11 @@ class PyPiService extends Serializable {
     extractOpt[PyPiProject](json)
   }
 
+  /** Returns a project as a raw string.
+    *
+    * @param endpoint the end_point to do the request.
+    * @return an optional String.
+    */
   def getProjectRaw(endpoint: String): Option[String] = {
     val response = try {
       val request = Http(url + endpoint).headers(getHeaders)
