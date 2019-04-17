b869519b46886183df19f170e75355a1ab14b779
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 11:13:39 +0200

    Change to async function.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
@@ -1,19 +1,32 @@
 package org.codefeedr.plugins.pypi.stages
 
+import java.util.concurrent.TimeUnit
+
 import org.apache.flink.api.common.functions.RichMapFunction
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.scala.{AsyncDataStream, DataStream}
 import org.codefeedr.plugins.pypi.protocol.Protocol.{PyPiProject, PyPiRelease}
 import org.codefeedr.stages.TransformStage
 import org.apache.flink.api.scala._
-
+import org.apache.flink.streaming.api.scala.async.{AsyncFunction, ResultFuture}
 class PyPiReleaseExtStage(stageId: String = "pypi_releases")
     extends TransformStage[PyPiRelease, PyPiProject](Some(stageId)) {
   override def transform(
       source: DataStream[PyPiRelease]): DataStream[PyPiProject] = {
-    source.map(new MapReleaseToProject)
+
+    AsyncDataStream.orderedWait(source,
+                                new MapReleaseToProject,
+                                5,
+                                TimeUnit.SECONDS,
+                                100)
   }
 }
 
-class MapReleaseToProject extends RichMapFunction[PyPiRelease, PyPiProject] {
-  override def map(value: PyPiRelease): PyPiProject = {}
+/** Maps a [[PyPiRelease]] to a [[PyPiProject]]. */
+class MapReleaseToProject extends AsyncFunction[PyPiRelease, PyPiProject] {
+
+  override def asyncInvoke(input: PyPiRelease,
+                           resultFuture: ResultFuture[PyPiProject]): Unit = {}
+
+  override def timeout(input: PyPiRelease,
+                       resultFuture: ResultFuture[PyPiProject]): Unit = {}
 }
