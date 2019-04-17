d046bc3906468034ca2742d5d1a5c627168d62ff
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 11:01:56 +0200

    Started on ext releases stage impl.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
@@ -1,3 +1,11 @@
 package org.codefeedr.plugins.pypi.stages
 
-class PyPiReleaseExtStage {}
+import org.apache.flink.streaming.api.scala.DataStream
+import org.codefeedr.plugins.pypi.protocol.Protocol.{PyPiProject, PyPiRelease}
+import org.codefeedr.stages.TransformStage
+
+class PyPiReleaseExtStage(stageId: String = "pypi_releases")
+    extends TransformStage[PyPiRelease, PyPiProject](Some(stageId)) {
+  override def transform(
+      source: DataStream[PyPiRelease]): DataStream[PyPiProject] = {}
+}
