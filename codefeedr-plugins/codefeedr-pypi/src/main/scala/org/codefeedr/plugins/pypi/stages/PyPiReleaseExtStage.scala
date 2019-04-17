917525ca83fd48bd49a50b2e66549460ab366043
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-17 11:05:06 +0200

    Add custom map function.

--- codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
+++ codefeedr-plugins/codefeedr-pypi/src/main/scala/org/codefeedr/plugins/pypi/stages/PyPiReleaseExtStage.scala
@@ -1,11 +1,19 @@
 package org.codefeedr.plugins.pypi.stages
 
+import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.streaming.api.scala.DataStream
 import org.codefeedr.plugins.pypi.protocol.Protocol.{PyPiProject, PyPiRelease}
 import org.codefeedr.stages.TransformStage
+import org.apache.flink.api.scala._
 
 class PyPiReleaseExtStage(stageId: String = "pypi_releases")
     extends TransformStage[PyPiRelease, PyPiProject](Some(stageId)) {
   override def transform(
-      source: DataStream[PyPiRelease]): DataStream[PyPiProject] = {}
+      source: DataStream[PyPiRelease]): DataStream[PyPiProject] = {
+    source.map(new MapReleaseToProject)
+  }
+}
+
+class MapReleaseToProject extends RichMapFunction[PyPiRelease, PyPiProject] {
+  override def map(value: PyPiRelease): PyPiProject = {}
 }
