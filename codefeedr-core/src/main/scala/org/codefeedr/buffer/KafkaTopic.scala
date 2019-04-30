4845fcea7a934505ad3f3355d82e62ceadfc787b
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-30 11:32:43 +0200

    Some Scaladoc.

--- codefeedr-core/src/main/scala/org/codefeedr/buffer/KafkaTopic.scala
+++ codefeedr-core/src/main/scala/org/codefeedr/buffer/KafkaTopic.scala
@@ -6,9 +6,11 @@ import org.codefeedr.pipeline.Context
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
 
+/** KafkaTopic stage which only embodies the topic name, to let a linked stage read from this topic. */
 class KafkaTopic[T <: Serializable with AnyRef: ClassTag: TypeTag](
     topic: String)
     extends FakeStage[T](topic) {
 
+  /** This will never be run. */
   override def main(context: Context): DataStream[T] = ???
 }
